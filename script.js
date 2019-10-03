d47082cf8840aca9963d7f3eb91f341937c43cb6
Ben Grzybowski <bengrzybowski@Bens-MacBook-Pro.local>
2019-10-03 11:43:14 -0400

    added about and contact

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 //const apiurl=`https://api.edamam.com/search?q=chicken,tomato,mozzarella&app_id=99c40f2e&app_key=a3d4da4b17ae857fc24756ac89d98778&excluded=garlic`;
 const APIURL = `https://api.edamam.com/search?app_id=99c40f2e&app_key=a3d4da4b17ae857fc24756ac89d98778&q=`;
 const button = document.querySelector("#search-button");
+const contactButton = document.querySelector("#contact-submit");
 
 document.querySelector("#searchForm").style.visibility = "hidden";
-buildRecipes("Recommendations", getRandomIngredients(3), 5);
+document.querySelector(".contact-container").style.visibility = "hidden";
+
+buildRecipes("Recommendations", getRandomIngredients(2), 5);
 function showSearch() {
   document.querySelector("#searchForm").style.visibility = "visible";
   document.querySelector("#search-form-top").style.visibility = "hidden";
@@ -13,6 +16,15 @@ button.addEventListener("click", async function () {
   if (query.length === 0) return;
   buildRecipes("Search Results", query, 10);
   document.querySelector('#data-section').scrollIntoView({ behavior: 'smooth' });
+})
+function showContact() {
+  document.querySelector(".contact-container").style.visibility = "visible";
+}
+function showAbout() {
+  alert("FoodForThought by Ben Grzybowski 2019.");
+}
+contactButton.addEventListener("click", async function () {
+  document.querySelector(".contact-container").style.visibility = "hidden";
 })
 
 async function buildRecipes(lblResults, queryString, numOfResults) {
@@ -49,20 +61,26 @@ async function buildRecipes(lblResults, queryString, numOfResults) {
 }
 
 function validateInput() {
+  //get included and excluded lists from user input
   let includedFood = document.getElementById("included-food").value;
   let excludedFood = document.getElementById("excluded-food").value;
+  //alert the user that they must add one item
   if (includedFood.length < 1) {
     alert("You need to enter at least one food item.", "FOOD for THOUGHT");
     return '';
   }
+  //split the included list to and array
   let inc = includedFood.split('\n');
-
+  //variable for return value
   let retVal = "";
+  //loop through the included list, replacing an empty string with a '+'
   for (var i = 0; i < inc.length; i++) {
     retVal += inc[i].replace(" ", "+") + ","
   }
+  //strip the trailing comma
   retVal = retVal.substring(0, retVal.length - 1);
 
+  //loop through the included list, replacing an empty string with a '+'
   if (excludedFood.length > 0) {
     retVal += "&excluded=";
     let exc = excludedFood.split('\n');
@@ -74,19 +92,28 @@ function validateInput() {
   return retVal;
 }
 function getRandomIngredients(num) {
+  //Array of ingredients
   const arrIngredients = [
     'Beef', 'Beans', 'Chicken', 'Chocolate', 'Apple', 'orange', 'venison', 'Mushrooms',
     'Pasta', 'Pork', 'Potatoes', 'Poultry', 'Rice', 'Salmon', 'Seafood', 'Shrimp',
     'Tofu', 'Tempeh', 'Turkey'];
+  //variable for return value
   let ingredientsList = "";
+  //selected ingredients array. this is to get distinct items in return
   let selectedIngredients = [];
+  //loop on the number supplied in the input
   for (let i = 0; i < num; i++) {
+    //get a random ingredient from ingredients array
     let selectedIngredient = arrIngredients[Math.floor(Math.random() * arrIngredients.length)];
+    //logic to ensure I get a distinct ingredient
     while (selectedIngredients.includes(selectedIngredient)) {
       selectedIngredient = arrIngredients[Math.floor(Math.random() * arrIngredients.length)];
     }
+    //add new ingredient to return list
     ingredientsList += selectedIngredient;
+    //add new ingredient to selected array
     selectedIngredients.push(selectedIngredient);
+    //add a comma if it is not the last item
     if ((num - 1) !== i) ingredientsList += ',';
   }
   return ingredientsList;
